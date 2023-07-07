26554378a982b0228ece370536eca0c4958ff1fc
jeffubayi <jeffubayi@gmail.com>
2023-07-07 15:14:10 +0300

    add next image domains

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "pickupmtaani.com",
+      'd38vok79sctage.cloudfront.net',
+      'img.freepik.com',
+      'icons-for-free.com'
+    ],
+  },
+}
+
+module.exports = nextConfig

cd9901d88f87376faadc34cab9643e762cb655b0
jeffubayi <jeffubayi@gmail.com>
2023-07-07 15:21:20 +0300

    add themes

---
+++ src/config/theme/palette-base.ts
@@ -0,0 +1,18 @@
+import { PaletteOptions } from '@mui/material'
+
+const paletteBase: Partial<PaletteOptions> = {
+  primary: {
+    light: '#9FF1D2',
+    main: '#FFC122',
+    dark: '#0D6A69',
+    contrastText: '#000',
+  },
+  secondary: {
+    light: '#FDE6A8',
+    main: '#ffaf35',
+    dark: '#D0821C',
+    contrastText: '#2A444E',
+  },
+}
+
+export default paletteBase

bf84e720124959b7251be9306fe183e5bba68f42
jeffubayi <jeffubayi@gmail.com>
2023-07-07 16:06:14 +0300

    add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Avatar, Box, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
+import { Avatar, Box, Link, ListItemAvatar, ListItemText } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
@@ -15,8 +15,8 @@ const Footer: FC = () => {
         <Grid container spacing={1}>
           <Grid item xs={12} md={5}>
             <Box sx={{ width: { xs: '100%', md: 360 }, mb: { xs: 3, md: 0 } }}>
-                  <Avatar src="/images/light-logo.png" sx={{ mb: 1 }} />
-               
+              <Avatar src="/images/light-logo.png" sx={{ mb: 1 }} />
+
               <Typography variant="subtitle1" sx={{ letterSpacing: 1, mb: 2 }}>
                 Pickup mtaani is an online logistics platform that is trusted by online sellers and buyers since the COVID pandemic until now.
               </Typography>
@@ -28,6 +28,13 @@ const Footer: FC = () => {
           </Grid>
         </Grid>
       </Container>
+      <Typography variant="body2" align="center" sx={{ mt: 2 }}>
+        {'Copyright © '}
+        <Link color="primary" href="https://github.com/jeffubayi">
+          jeffubayi
+        </Link>{""}{' All rights reserved '}
+        {'.'}
+      </Typography>
     </Box>
   )
 }
